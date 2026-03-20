e94b398a397b5ed7dfe8c7c986d4f807cd143aed
Kenyi Kubari <kenyikubari@outlook.com>
2026-03-19 19:25:14 -0500

    Minimal working example of rust arduino compatibility established.

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,13 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
-    let mut led = pins.d13.into_output();
-
+  
+    let mut led13 = pins.d13.into_output();
+    let mut led12 = pins.d12.into_output();
+    
     loop {
-        led.toggle();
+        led13.toggle();
         arduino_hal::delay_ms(1000);
+        led12.toggle();
     }
 }
